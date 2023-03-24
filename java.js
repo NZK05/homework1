2e27940905786a62ebb1722227dfef761ab248ce
NZK05 <128840858+NZK05@users.noreply.github.com>
2023-03-25 00:44:08 +0400

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
 
+const row = 4;
+const col = 5;
+let numbers = [];
+
+for (let i = 0; i < row; i++) {
+  numbers.push([]);
+}
+
+let count = 1;
+for (let j = 0; j < col; j++) {
+  for (let i = 0; i < row; i++) {
+    numbers[i][j] = count;
+    count++;
+  }
+ 
+}
+
+console.log(numbers);
